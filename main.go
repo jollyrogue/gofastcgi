01c113e915d06dbfb1754c647a2f0b20c6a82fe5
Ryan Quinn <jollyrogue@dangertoaster.com>
2019-08-01 14:23:14 +0000

    Checkpointing work.
     On branch master
     Changes to be committed:
    	new file:   main.go

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/http/fcgi"
+	"runtime"
+
+	"github.com/gorilla/mux"
+)
+
+var local string
+var tcp string
+var unix string
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	flag.StringVar(&local, "local", "",
+		"serve as webserver, example: 0.0.0.0:8000")
+	flag.StringVar(&tcp, "tcp", "",
+		"serve as FCGI via TCP, example: 0.0.0.0:8000")
+	flag.StringVar(&unix, "unix", "",
+		"serve as FCGI via UNIX socket, example: /tmp/myprogram.sock")
+}
+
+func homeView(w http.ResponseWriter, r *http.Request) {
+	headers := w.Header()
+	headers.Add("Content-Type", "text/html")
+	io.WriteString(w, "<html><head></head><body><p>It works!</p></body></html>")
+}
+
+func main() {
+	flag.Parse()
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", homeView)
+
+	var err error
+
+	if local != "" { // Run as a local web server
+		log.Printf("Running builtin webserver at %s...\n", local)
+		err = http.ListenAndServe(local, r)
+	} else if tcp != "" { // Run as FCGI via TCP
+		log.Printf("Running as FastCGI TCP at %s...", tcp)
+		listener, err := net.Listen("tcp", tcp)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer listener.Close()
+
+		err = fcgi.Serve(listener, r)
+	} else if unix != "" { // Run as FCGI via UNIX socket
+		log.Printf("Running as FastCGI Unix socket at %s...\n", unix)
+		listener, err := net.Listen("unix", unix)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer listener.Close()
+
+		err = fcgi.Serve(listener, r)
+	} else { // Run as FCGI via standard I/O
+		log.Printf("Running as FastCGI standard IO...\n")
+		err = fcgi.Serve(nil, r)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Shutting down.")
+}
